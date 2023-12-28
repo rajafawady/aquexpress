439086e7a9a50ff70599a90b42c5faea93a35124
Raja Fawad <rajafawady@gmail.com>
2023-12-28 20:10:07 +0500

    Changed stats

--- app/Http/Controllers/customerController.php
+++ app/Http/Controllers/customerController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class CustomerController extends BaseController
 {
