0809d40095f2b3d962f9df84721397c09b0d95f5
am-i-hira <hiratoheed2002@gmail.com>
2023-12-28 19:33:06 +0500

    authentication of customer

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,42 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+    public function authenticate(Request $request){
+        $formFields=$request->validate([
+            'email'=> ['required','email'],
+            'password'=>'required',
+        ]);
+        if(Auth::attempt($formFields)){
+            $request->session()->regenerate();
+            return redirect('/')->with('message', 'You are logged in as a Supplier!');
+        }
+
+        return back()->withErrors(['email' => 'Invalid Credentials'])->onlyInput('email');
+    }
+
+    public function createUser(Request $request){
+        $formFields=$request->validate(
+            [
+                'companyName'=>'required',
+                'phone'=>'required',
+                'email'=>['required','email', Rule::unique('suppliers', 'email')],
+                'password'=>'required | confirmed | min:6',
+                'address'=>'required',
+            ]
+            );
+            // Hash Password
+        $formFields['password'] = bcrypt($formFields['password']);
+        // Create User
+        $user = User::create($formFields);
+        Auth::attempt($user);
+        return redirect('/')->with('message', 'User created and logged in');
+     }
+
+    public function logout(Request $request){
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/login')->with('message', 'You have been logged out!');
+    }
 }
